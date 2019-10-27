af8242e498456f381611ce9993f61ce9ff733317
Pavel129 <chekov_pavel_@mail.ru>
2019-10-27 15:27:39 +0300

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Mac on 27/10/2019.
+//  Copyright © 2019 PAVEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
