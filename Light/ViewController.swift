6f94da16b59284bb9573efac82e1dc8176d04a7d
Pavel129 <chekov_pavel_@mail.ru>
2019-10-28 04:12:15 +0300

    version 1.2

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -5,16 +5,47 @@
 //  Created by Mac on 27/10/2019.
 //  Copyright © 2019 PAVEL. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = false
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+
+           lightOn = !lightOn
+           
+    }
+    
+     
+ func updateView() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = lightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = lightOn ? .white : .black
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("privet")
+       updateView()
     }
 
 
